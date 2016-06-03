d3a6f6d534da0c0c33edecefd4140b7e24b0621c
Alberto Mijares <almilo@almilo.es>
2016-06-03 18:05:08 +0200

    (fix): add support for Object.assign() through polyfill

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+// required in old node versions because of Object.assign() usage
+import 'babel-polyfill';
+
 export { default as AnnotatedGraphQLSchemaFactory } from './annotated-graphql-schema-factory'
